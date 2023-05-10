61629644bfcff3454fbc72186051d695a1992438
winnieSikinyi <winniesikinyi@gmail.com>
2023-05-10 10:06:17 +0300

    asynch

--- asynchassgn.js
+++ asynchassgn.js
@@ -10,6 +10,28 @@ let promise = new Promise(function(resolve,reject){
     }
 });
 
+let userDetails = "I'm comming";
+
+let promse = new Promise(function(resolve,reject){
+    if (userDetails){
+        setTimeout(resolve(`user details available`),5000);
+
+    }else{
+        setTimeout(()=>{reject(` No user details available`)},5000);
+    }
+});
+let add =()=>{
+    console.log(20+30);
+}
+add();
+
+const getUserDetails = async ()=>{
+    let response = await promise;
+    console.log({response});
+    
+}
+getUserDetails();
+
 // // You have an array of user IDs and a function getUserData(id)
 // //  that returns a Promise with user data when given a user ID. 
 // //  Write an asynchronous function that fetches and logs the data
@@ -19,7 +41,7 @@ const ownIds = [13, 12, 37, 42, 25];
 
 async function fetchAndLogUserData() {
   for (const id of ownIds  ) {
-    const userData = await getUserData(id);
+    const userData = await ownIds(id);
     console.log(userData);
   }
 }
@@ -31,27 +53,27 @@ fetchAndLogUserData();
 //   Write a function that calls performTask() and logs a custom 
 //   success message if the task is successful, and a custom error message 
 //   if there's an error.
+function canHandle(){
+  doTask()
+  .then(()=>{
+    console.log("Yeeeeey!!!! task completed successfuly!!");
+  })
+  .catch(()=>{
+    console.log("oops task failed terribly")
 
-function handleTask() {
-    performTask()
-      .then(() => {
-        console.log("Task completed successfully!");
-      })
-      .catch(() => {
-        console.log("Task failed!");
-      });
-  };
-  function performTask() {
-    return new Promise((resolve, reject) => {
-      // Perform some asynchronous task
-      const success = true;
-      if (success) {
-        resolve();
-      } else {
-        reject();
-      }
-    });
+  });
+};
+
+function doTask(){
+return new Promise((resolve,reject) =>{
+  const elegant = true;
+  if(elegant){
+    resolve();
+  }else{
+    reject();
   }
-  
-  
-handleTask();
+});
+}
+canHandle();
+
+
